6a4d88323a10a46c13126caeaad8d1774763943a
samyak-19 <samlinkin19@gmail.com>
2026-04-22 23:20:15 +0530

    Day 4: Gateway test integration

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,37 @@ function App() {
       body: JSON.stringify({ email, password }),
     });
     const data = await res.json();
+    localStorage.setItem("token", data.token);
     console.log(data);
   };
+
+  const createApi = async () => {
+  const token = localStorage.getItem("token");
+
+  const res = await fetch("http://localhost:5000/api/apis/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": token,
+    },
+    body: JSON.stringify({
+      name: "Test API",
+      baseUrl: "https://jsonplaceholder.typicode.com",
+    }),
+  });
+
+  const data = await res.json();
+  console.log(data);
+};
+
+const callApi =async() =>{
+  const key = "b95aa147f8f3d6725b6118d7e7b0a14be244b4eb285597e77d3e5256327bdeb6";
+  const res= await fetch(
+    `http://localhost:5000/gateway/${key}/pokemon/ditto`
+  );
+   const data = await res.json();
+  console.log(data);
+}
 
   return (
     <div className="p-10 space-y-4">
